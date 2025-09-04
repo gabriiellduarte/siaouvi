c67aaa84f71c3cb96ea023d090c699b126b5fd01
Gabriel Duarte <gduarte.plus@gmail.com>
2025-09-04 15:01:08 -0300

    base alterada

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OuvidoriaController;
+use App\Models\Manifestacao;
 
-Route::get('/ouvidoria', [OuvidoriaController::class, 'create'])->name('ouvidoria.form');
-Route::post('/ouvidoria', [OuvidoriaController::class, 'store'])->name('ouvidoria.store');
-Route::get('/show/{id}', [OuvidoriaController::class, 'show'])->name('ouvidoria.show');
-Route::get('/edicao/{id}', [OuvidoriaController::class, 'edit'])->name('ouvidoria.edicao');
-Route::delete('/delete/{id}', [OuvidoriaController::class, 'destroy'])->name('ouvidoria.destroy');
-Route::get('/dashboard', [OuvidoriaController::class, 'dashboard'])->name('dashboard');
-Route::post('/update/{id}', [OuvidoriaController::class, 'update'])->name('ouvidoria.update');
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('principal');
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+
+    //SISTEMA ANTIGO
+    Route::get('/ouvidoria', [OuvidoriaController::class, 'create'])->name('ouvidoria.form');
+    Route::post('/ouvidoria', [OuvidoriaController::class, 'store'])->name('ouvidoria.store');
+    Route::get('/show/{id}', [OuvidoriaController::class, 'show'])->name('ouvidoria.show');
+    Route::get('/edicao/{id}', [OuvidoriaController::class, 'edit'])->name('ouvidoria.edicao');
+    Route::delete('/delete/{id}', [OuvidoriaController::class, 'destroy'])->name('ouvidoria.destroy');
+    Route::get('/ouvi/dashboard', [OuvidoriaController::class, 'dashboard'])->name('dashboardouvi');
+    Route::post('/update/{id}', [OuvidoriaController::class, 'update'])->name('ouvidoria.update');
+    
+    Route::get('/ouvidoriacadastrar', function () {
+        $manifestacoes = Manifestacao::all();
+        return view('dashboard', compact('manifestacoes'));
+    });
+
 });
 
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
